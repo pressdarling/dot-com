e0d483fc9cf254c918ffb2c9bb3a1e4e3dbd5c40
pressdarling <32807920+pressdarling@users.noreply.github.com>
2024-07-27 21:27:33 +1000

    accessibility! working CSS! a slight re-import! and a whole lot of linting.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [tailwind({
+    // Optionally, add any Tailwind-specific options here
+    config: { path: './tailwind.config.mjs' },
+    applyBaseStyles: false,
+  })],
+}); 
