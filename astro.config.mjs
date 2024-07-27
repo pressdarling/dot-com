d6c828efc8d60ff5bbd63531a204cf4540d985b4
pressdarling <32807920+pressdarling@users.noreply.github.com>
2024-07-28 07:59:26 +1000

    testing updates
    yay

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [tailwind({
     // Optionally, add any Tailwind-specific options here
     config: { path: './tailwind.config.mjs' },
     applyBaseStyles: false,
   })],
+  site: 'https://bradyclarke.com',
 }); 
