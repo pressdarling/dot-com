63776b46ec7ac0e8ba969d71d19ce42ded73cd38
pressdarling <32807920+pressdarling@users.noreply.github.com>
2024-07-29 14:27:22 +1000

    again!

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,8 @@
-import globals from 'globals';
 import eslint from '@eslint/js';
 import eslintPluginAstro from 'eslint-plugin-astro';
 
 export default [
   eslint.configs.recommended,
-  {
-    languageOptions: { 
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-  },
   {
     files: ['**/*.{js,mjs,cjs,ts,astro}'],
     rules: {
