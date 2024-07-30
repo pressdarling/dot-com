791c6a8f3feef37b75fe479947adcbbe6dd6198a
pressdarling <32807920+pressdarling@users.noreply.github.com>
2024-07-30 21:22:34 +1000

    eslint config fixed?

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import eslint from '@eslint/js';
 import eslintPluginAstro from 'eslint-plugin-astro';
 
-export default [
+export default ([
   eslint.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs,ts,astro}'],
@@ -10,8 +10,12 @@ export default [
       'prefer-const': 'error',
     },
   },
+    ...eslintPluginAstro.configs.recommended,
   {
     files: ['**/*.astro'],
-    ...eslintPluginAstro.configs.recommended,
-  },
-];
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    }
+  }
+]);
